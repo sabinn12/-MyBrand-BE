ceb0b2bd50daba6dfe8a26d21cb4ce45e702109d
sabinn12 <irakizasabin12@gmail.com>
2024-03-19 10:12:04 +0200

    update

--- compile/jwt/jwt.js
+++ compile/jwt/jwt.js
@@ -1,21 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = require("jsonwebtoken");
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || 'default_secret';
 // import user from "../models/user";
 const createToken = (user) => {
-    const accessToken = (0, jsonwebtoken_1.sign)({ email: user.email }, "akarimakagasizondaatwa");
+    const accessToken = (0, jsonwebtoken_1.sign)({ email: user.email }, accessTokenSecret);
     console.log(accessToken);
     return accessToken;
 };
 const tokenValidation = (req, res, next) => {
     const accessToken = req.cookies["access-token"];
     if (!accessToken) {
-        return res.status(400).json({ error: "⚠️ you must log in with your account first" });
+        return res.status(400).json({ error: "you must log in with your account first" });
     }
     else {
         try {
             let authenticated;
-            const validToken = (0, jsonwebtoken_1.verify)(accessToken, "akarimakagasizondaatwa");
+            const validToken = (0, jsonwebtoken_1.verify)(accessToken, accessTokenSecret);
             if (validToken) {
                 authenticated = true;
                 return next();
--- src/jwt/jwt.ts
+++ src/jwt/jwt.ts
@@ -1,8 +1,9 @@
 import { sign,verify } from "jsonwebtoken";
 import {  Request,Response, NextFunction} from "express";
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || 'default_secret';
 // import user from "../models/user";
 const createToken = (user:any) => {
-const accessToken = sign({ email:user.email},"akarimakagasizondaatwa")
+    const accessToken = sign({ email:user.email}, accessTokenSecret);
 console.log(accessToken)
 return accessToken;
 
@@ -11,11 +12,11 @@ return accessToken;
 const tokenValidation  = (req:Request,res:Response,next:NextFunction) => {
     const accessToken  = req.cookies["access-token"];
     if(!accessToken){
-        return res.status(400).json({error:"⚠️ you must log in with your account first"})
+        return res.status(400).json({error:"you must log in with your account first"})
     }else{
         try{
             let authenticated:any;
-            const validToken = verify(accessToken,"akarimakagasizondaatwa");
+            const validToken = verify(accessToken, accessTokenSecret);
             if(validToken){
                 authenticated = true
                 return next();
