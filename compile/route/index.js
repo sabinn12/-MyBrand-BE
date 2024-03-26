ad81c037b0fd0012d54f13b07604490cfeb5093a
sabinn12 <irakizasabin12@gmail.com>
2024-03-26 13:04:25 +0200

    updated

--- compile/route/index.js
+++ compile/route/index.js
@@ -4,15 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const blog_routes_1 = __importDefault(require("./blog.routes"));
 const likeroute_1 = __importDefault(require("./likeroute"));
 const commentroutes_1 = __importDefault(require("./commentroutes"));
 const querriesroutes_1 = __importDefault(require("./querriesroutes"));
 const usersroutes_1 = __importDefault(require("./usersroutes"));
 const router = express_1.default.Router();
+// Enable CORS middleware
+router.use((0, cors_1.default)());
 router.use('/blogs', blog_routes_1.default);
 router.use('/blogs', likeroute_1.default);
 router.use('/blogs', commentroutes_1.default);
-router.use("/brand", querriesroutes_1.default);
-router.use("/brand", usersroutes_1.default);
+router.use('/brand', querriesroutes_1.default);
+router.use('/brand', usersroutes_1.default);
 exports.default = router;
--- src/route/index.ts
+++ src/route/index.ts
@@ -1,17 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 import blogRoute from './blog.routes';
-import likeroute from './likeroute'
+import likeroute from './likeroute';
 import commentRoutes from './commentroutes';
 import querriesRoutes from './querriesroutes';
 import usersRoutes from './usersroutes';
 
 const router = express.Router();
 
-router.use('/blogs',blogRoute);
-router.use('/blogs',likeroute);
-router.use('/blogs',commentRoutes);
-router.use("/brand",querriesRoutes);
-router.use("/brand",usersRoutes);
+// Enable CORS middleware
+router.use(cors());
 
+router.use('/blogs', blogRoute);
+router.use('/blogs', likeroute);
+router.use('/blogs', commentRoutes);
+router.use('/brand', querriesRoutes);
+router.use('/brand', usersRoutes);
 
 export default router;
